0c6833b063c9c44f7354d5cb11e04251452d7363
SanYurich <aleksandr.isupov@hotmail.com>
2022-05-08 08:15:40 +0300

    написал код программы, задачу решает

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿string [] incomingArray = {"One", "Two", "Three", "2", "54664", "+-/*>?", ".>"};
+
+void PrintArray (string [] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        if (arrayToPrint[i] != "")
+        {
+            Console.Write($" {arrayToPrint[i]},");
+        }
+    }
+    Console.Write("]");
+}
+
+string [] NoMoreThreeSymbolsInArray(string [] dirtyArray)
+{
+    string[] exitingArray = new string [dirtyArray.Length];
+    for (int i = 0; i < dirtyArray.Length; i++)
+    {
+        string random = dirtyArray[i];
+        if (random.Length <= 3)
+        {
+            exitingArray[i] = dirtyArray[i];
+        }
+        else
+        {
+            exitingArray[i] = "";
+        }
+    }
+    return exitingArray;
+}
+
+PrintArray(incomingArray);
+string [] showcaseArray = NoMoreThreeSymbolsInArray(incomingArray);
+Console.Write(" -> ");
+PrintArray(showcaseArray);
